aa0f6a61974aa8b595a45ccf9ae8d50fa5666f7d
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-03-27 17:31:14 +0100

    Bank class created

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,6 +1,8 @@
 require_relative '../lib/bank'
 
 describe Bank do
-  it '' do
+  it 'contains no bank accounts when created' do
+    bank = Bank.new
+    expect(bank.accounts).to match_array([])
   end
 end
