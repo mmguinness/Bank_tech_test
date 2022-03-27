10c22adff2a1fc9eaf62e5f2a6828c172d476da0
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-03-27 18:19:13 +0100

    Account class created. Bank stores accounts in array.

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,9 +1,13 @@
 class Bank
 
-attr_reader :accounts
+  attr_reader :accounts
 
   def initialize
     @accounts = []
+  end
+
+  def create_account(_account_name)
+    @accounts << (account_name = Account.new)
   end
 
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,8 +1,26 @@
 require_relative '../lib/bank'
+require_relative '../lib/account'
 
 describe Bank do
+  subject(:bank) { described_class.new }
+
   it 'contains no bank accounts when created' do
-    bank = Bank.new
     expect(bank.accounts).to match_array([])
   end
+  
+  describe '#accounts' do    
+    it 'can create a new bank account' do
+      bank.create_account('first_account')
+      expect(bank.accounts).to include(an_instance_of(Account))
+    end
+
+    let(:account) { double :account }
+    it 'can store multiple bank accounts' do
+      bank.create_account(account)
+      bank.create_account(account)
+      bank.create_account(account)
+      expect(bank.accounts.length).to be(3)
+    end
+  end
+
 end
