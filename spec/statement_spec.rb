3ca8e9561bc7122dfccca38fcc57d278210b45db
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-03-28 15:35:25 +0100

    Test passing for acceptance criteria of printed statement

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -28,4 +28,20 @@ describe Statement do
       expect { statement.print_statement(account) }.to output(output).to_stdout
     end
   end
+
+  it 'returns the transactions of a given account, in reverse order,' do
+    account = Account.new
+    account.credit(1000, '10/01/2023')
+    account.credit(2000, '13/01/2023')
+    account.debit(500, '14/01/2023')
+
+    output = 
+"date || credit || debit || balance
+14/01/2023 || || 500.00 || 2500.00
+13/01/2023 || 2000.00 || || 3000.00
+10/01/2023 || 1000.00 || || 1000.00
+"
+    expect { statement.print_statement(account) }.to output(output).to_stdout
+  end
+
 end
